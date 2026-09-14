f3b552cce28d39981faa001b96c2958ce2f14317
ROJSUWAN <100037765+ROJSUWAN@users.noreply.github.com>
2026-09-14 21:16:03 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const C='loan-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png',
+ 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(e.request.method==='GET'&&res.ok){const cp=res.clone();caches.open(C).then(c=>c.put(e.request,cp))}return res}).catch(()=>r)))});
